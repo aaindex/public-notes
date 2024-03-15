00e842cfb6afddb52bb752c5f34e295b46297210
Willaim Li <aaindex@gmail.com>
2024-03-14 20:40:51 -0400

    Add generate-oauth2-access-token-1.sh

---
+++ curl-for-sso/generate-oauth2-access-token-1.sh
@@ -0,0 +1,2 @@
+curl -v -k "https://localhost:9443/oauth2/authorize?response_type=code&client_id=AodKBACDZTYwzXa24IUCboyOrhGO8a&redirect_uri=https://localhost:8082/login/oauth2/code/oauth2-login-8082&scope=openid"
+
